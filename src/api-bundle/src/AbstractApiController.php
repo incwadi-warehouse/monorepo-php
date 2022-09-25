ee7f2b527666bc3f69b2d7ba2d76a457fa86ab0d
André Baldeweg <a@baldeweg.org>
2022-09-25 18:59:05 +0000

    Remove response from controller

--- src/api-bundle/src/AbstractApiController.php
+++ src/api-bundle/src/AbstractApiController.php
@@ -4,15 +4,9 @@ namespace Baldeweg\Bundle\ApiBundle;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Baldeweg\Bundle\ApiBundle\Serializer;
 
 abstract class AbstractApiController extends AbstractController
 {
-    protected function setResponse()
-    {
-        return new Response(new Serializer());
-    }
-
     protected function submitForm(Request $request): array
     {
         return json_decode(
